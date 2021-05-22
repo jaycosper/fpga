9475fe45c86584945bf78c53b6a7c5a9354629ca
Jay Cosper <jaycosper@gmail.com>
2021-05-21 19:27:07 -0700

    Updating documentation

--- sandbox/clk_pulse/clk_pulse.v
+++ sandbox/clk_pulse/clk_pulse.v
@@ -1,40 +1,39 @@
 // Clock pulse and edge detection generator
 // Clock pulse if rounded to nearest log2 value
 module clk_pulse #(
-    parameter PULSE_CYCLE_COUNT = 1000
+    //! Number of clock cycles per pulse
+    parameter integer PULSE_CYCLE_COUNT = 1000
 )(
-    input wire clk,
-    input wire rst_n,
-    input wire data,
-    output reg clk_pulse,
-    output reg data_redge,
-    output reg data_fedge
+    input wire clk,         //! input clock
+    input wire rst_n,       //! active-low asynchronous reset
+    input wire data,        //! data for edge detection
+    output reg clk_pulse,   //! clock pulse output, one clock-cycle wide
+    output reg data_redge,  //! rising-edge detection pulse of data
+    output reg data_fedge   //! falling-edge detection pulse of data
 );
 
+    //! Calculate the size of the counter
     localparam PULSE_CNTR_WIDTH = $clog2(PULSE_CYCLE_COUNT);
 
+    //! clock pulse counter
     reg [PULSE_CNTR_WIDTH-1:0] pulse_cntr;
+    //! clocked in data
     reg data_q, data_qq;
 
-    //function integer log2;
-    //    input [31:0] value;
-    //    for (log2=-1; value>0; log2=log2+1)
-    //        value = value>>1;
-    //endfunction
-
+    //! generate the clock pulse every 2^PULSE_CYCLE_COUNT clocks
     always@(posedge clk or negedge rst_n) begin : clk_pulse_gen
         if (!rst_n) begin
             pulse_cntr <= 0;
             clk_pulse <= 1'b0;
         end else begin
-            // count clock pulses
+            // increment clock pulses
             pulse_cntr <= pulse_cntr + 1'b1;
             // set clock pulse when counter reset
             clk_pulse <= (pulse_cntr == 0) ? 1'b1 : 1'b0;
         end
     end
 
-    // Edge detection
+    //! Clocked edge detection
     always@(posedge clk or negedge rst_n) begin : clk_data
         if (!rst_n) begin
             data_q <= 1'b0;
@@ -45,6 +44,7 @@ module clk_pulse #(
         end
     end
 
+    //! Combinatorial edge-detection outputs
     always@(*) begin : edge_detect_gate
         // Rising edge detect
         data_redge <= (data_q == 1'b1 && data_qq == 1'b0) ? 1'b1 : 1'b0;
