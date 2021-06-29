4d055ee2939a500e50974b7e59f04dbaf6847d68
Jay Cosper <jaycosper@gmail.com>
2021-06-28 19:43:07 -0700

    Adding traditional PWM generator with WB interface

---
+++ sandbox/pwm_gen/pwm_gen.v
@@ -0,0 +1,76 @@
+/**
+ * Sine wave generator
+**/
+
+module pwm_gen
+#(
+    parameter WB_DATA_WIDTH = 8,
+    parameter PWM_WIDTH = 8,
+    parameter PWM_RATE_WIDTH = 8
+)(
+    input i_clk,
+    input i_rst,
+    input i_rst_n,
+
+    input i_wb_stb,
+    input i_wb_we,
+    input i_wb_addr,
+    input [WB_DATA_WIDTH-1:0] i_wb_wrdata,
+
+    output o_wb_ack,
+    output [WB_DATA_WIDTH-1:0] o_wb_rddata,
+
+    input [PWM_WIDTH-1:0] i_pwm_setpoint,
+    output o_pwm_out
+);
+    reg o_wb_ack;
+
+    reg [PWM_WIDTH-1:0] r_reload_value;
+    reg [WB_DATA_WIDTH-1:0] o_wb_rddata;
+    wire o_pwm_out;
+
+    always @(posedge i_clk) begin : WB_HANDLER // Data write
+        if (i_rst) begin
+            r_reload_value <= {PWM_WIDTH{1'b1}};
+            o_wb_ack <= 1'b0;
+            o_wb_rddata <= 0;
+        end else begin
+            o_wb_rddata <= 0;
+            if ((i_wb_stb)&&(i_wb_addr)) begin
+                if (i_wb_we) r_reload_value <= i_wb_wrdata[(PWM_WIDTH-1):0];
+                else o_wb_rddata[(PWM_WIDTH-1):0] <= r_reload_value;
+            end
+            o_wb_ack <= i_wb_stb;
+        end
+    end
+
+    reg [PWM_RATE_WIDTH-1:0] r_pulse_rate;
+    always @(posedge i_clk) begin : MULTI_RATE
+        if (i_rst) begin
+            r_pulse_rate <= 0;
+        end else begin
+            r_pulse_rate <= r_pulse_rate + 1'b1;
+        end
+    end
+
+    reg [PWM_WIDTH-1:0] r_pwm_cntr;
+    reg [PWM_WIDTH-1:0] r_pwm_setpoint;
+    always @(posedge i_clk) begin : PWM_GEN
+        if (i_rst) begin
+            r_pwm_cntr <= 0;
+            r_pwm_setpoint <= {PWM_WIDTH{1'b1}}>>1; // 50%
+        end else begin
+            if (r_pulse_rate == 0) begin
+                r_pwm_setpoint <= i_pwm_setpoint;
+                if (r_pwm_cntr >= r_reload_value) begin
+                    r_pwm_cntr <= 0;
+                end else begin
+                    r_pwm_cntr <= r_pwm_cntr + 1'b1;
+                end
+            end
+        end
+    end
+
+    assign o_pwm_out = (r_pwm_cntr>r_pwm_setpoint);
+
+endmodule
---
+++ sandbox/pwm_gen/pwm_gen_tb.v
@@ -0,0 +1,86 @@
+`timescale 1ns/100ps // 1 ns time unit, 100 ps resolution
+
+`define assert(signal, value) \
+        if (signal !== value) begin \
+            $display("ASSERTION FAILED in %m: signal != value"); \
+            $finish; \
+        end
+
+module pwm_gen_tb;
+
+    localparam  TB_WB_DATA_WIDTH = 8;
+    localparam  TB_PWM_WIDTH = 4;
+    localparam  TB_PWM_RATE_WIDTH = 1;
+
+    reg clk = 0;
+    always #5 clk = !clk;
+
+    reg rst;
+
+    reg wb_stb;
+    reg wb_we;
+    reg wb_addr;
+    reg [TB_WB_DATA_WIDTH-1:0] wb_wrdata;
+
+    wire wb_ack;
+    wire [TB_WB_DATA_WIDTH-1:0] wb_rddata;
+    reg [TB_PWM_WIDTH-1:0] pwm_setpoint;
+    wire pwm_out;
+
+    pwm_gen #(
+        .WB_DATA_WIDTH  ( TB_WB_DATA_WIDTH  ),
+        .PWM_WIDTH      ( TB_PWM_WIDTH      ),
+        .PWM_RATE_WIDTH ( TB_PWM_RATE_WIDTH )
+    ) u_pwm_gen (
+        .i_clk          ( clk           ),
+        .i_rst          ( rst           ),
+        .i_rst_n        ( 1'b1          ),
+        .i_wb_stb       ( wb_stb        ),
+        .i_wb_we        ( wb_we         ),
+        .i_wb_addr      ( wb_addr       ),
+        .i_wb_wrdata    ( wb_wrdata     ),
+        .o_wb_ack       ( wb_ack        ),
+        .o_wb_rddata    ( wb_rddata     ),
+        .i_pwm_setpoint ( pwm_setpoint  ),
+        .o_pwm_out      ( pwm_out       )
+    );
+
+    localparam TEST_LOOPS = 100;
+    integer testcase;
+    integer i = 0;
+    integer n = 0;
+
+    initial begin
+        $dumpfile("wave.vcd");      // create a VCD waveform dump called "wave.vcd"
+        $dumpvars(0, pwm_gen_tb);       // dump variable changes in the testbench
+                                    // and all modules under it
+
+        $display("time\trst\twb_stb\twb_we\twb_addr\twb_wrdata\twb_ack\twb_rddata\tpwm_out");
+        $monitor("%4g\t%b\t%b\t%b\t%x\t%x\t\t%b\t%x\t\t%b",
+                $time, rst, wb_stb, wb_we, wb_addr, wb_wrdata, wb_ack, wb_rddata, pwm_out);
+
+        // testcase #0 : reset
+        testcase = 0;
+        wb_stb <= 1'b0;
+        wb_we <= 1'b0;
+        wb_addr <= 0;
+        wb_wrdata <= 0;
+        pwm_setpoint <= {TB_PWM_WIDTH{1'b1}}>>1; // 50%
+
+        @(negedge clk);
+        @(negedge clk);
+        rst = 1;
+        @(negedge clk);
+        rst = 0;
+
+        // testcases
+        for (i = 0; i < TEST_LOOPS; i++) begin
+            // testcase number
+            testcase = i+1;
+            @(negedge clk);
+        end
+
+        $finish();
+
+    end
+endmodule
