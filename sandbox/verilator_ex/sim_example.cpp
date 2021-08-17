692ea1673f5bd130247210a8d303c25b547217ec
Jay Cosper <jaycosper@gmail.com>
2021-08-17 15:55:19 -0700

    added RISC-V toolchain path minor updates to verilator example

--- sandbox/verilator_ex/sim_example.cpp
+++ sandbox/verilator_ex/sim_example.cpp
@@ -3,17 +3,18 @@
 #include "verilated.h"
 
 int main(int argc, char **argv) {
-	// Initialize Verilators variables
-	Verilated::commandArgs(argc, argv);
+    // Initialize Verilators variables
+    Verilated::commandArgs(argc, argv);
 
-	// Create an instance of our module under test
-	Vexample *tb = new Vexample;
+    // Create an instance of our module under test
+    Vexample *tb = new Vexample;
+    tb->opentrace("trace.vcd");
 
-	// Tick the clock until we are done
-	while(!Verilated::gotFinish()) {
-		//tb->i_clk = 1;
-		//tb->eval();
-		//tb->i_clk = 0;
-		tb->eval();
-	} exit(EXIT_SUCCESS);
+    // Tick the clock until we are done
+    while(!Verilated::gotFinish()) {
+        //tb->i_clk = 1;
+        //tb->eval();
+        //tb->i_clk = 0;
+        tb->eval();
+    } exit(EXIT_SUCCESS);
 }
